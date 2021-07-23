2f8201d27d9339092670bda168a65744cc15e4df
Pranav-Rustagi <pranav001100@gmail.com>
2021-07-24 01:42:02 +0530

    layout handled for small screens

--- src/App.js
+++ src/App.js
@@ -78,41 +78,40 @@ const App = () => {
     }
 
     
-    return <Container className="h-100 row m-auto py-3 flex-lg-column justify-content-center overflow-hidden">
+    return <Container className="h-100 row m-auto py-3 flex-lg-column justify-content-center">
 
                 <header className="px-0 mb-3 shadow fit-content">
                     <h1 className="display-4 fw-bold text-light text-center bg-dark py-3 m-0 rounded-3">Tic-Tac-Toe</h1>
                 </header>
 
                 <Col>
-                    <Row className="h-100 justify-content-between flex-column-reverse flex-lg-row">
-                        <Col id="scoreContainer" className="p-4 d-flex shadow rounded-3" xs="12" lg="5">
+                    <Row className="h-100 justify-content-between flex-column-reverse flex-md-row">
+                        <Col id="scoreContainer" className="p-3 p-md-4 d-flex shadow rounded-3 mx-md-auto ms-md-0" xs="11" md="5">
                             <Row className="w-100 m-0 text-center flex-row flex-lg-column justify-content-between">
-                                <div>
-                                    <div className="text-light bg-dark fw-bold p-3 rounded">
-                                        <h1 className="fs-2">{ "Round " + roundCount }</h1>
-                                        <p className="lead m-0">
-                                            { 
-                                                gameWon ?
-                                                players[i] + " Won!" :
-                                                (gameTie ? "Game tie" :
-                                                "Current move : " + players[i])
-                                            }
-                                        </p>
-                                    </div>
+                                
+                                <div className="text-light bg-dark fw-bold py-2 p-md-3 rounded fit-content col-12">
+                                    <h1 className="fs-2">{ "Round " + roundCount }</h1>
+                                    <p className="lead m-0">
+                                        { 
+                                            gameWon ?
+                                            players[i] + " Won!" :
+                                            (gameTie ? "Game tie" :
+                                            "Current move : " + players[i])
+                                        }
+                                    </p>
                                 </div>
 
                                 {
-                                    gameWon ? <Col className="mx-auto" xs="6"><img className="w-100 drop-shadow" src={Win} alt="victory" /></Col> : ""
+                                    gameWon ? <Col className="m-auto d-none d-md-block" xs="4" md="10" lg="6"><img className="w-100 drop-shadow" src={Win} alt="victory" /></Col> : ""
                                 }
 
-                                <div>
+                                <div className="fit-content mt-2 mt-md-auto p-0 col-12">
                                     <ScoreBoard score={ score } />
                                 </div>
                             </Row>
                         </Col>
                         
-                        <Col className="d-flex ms-3 flex-row flex-lg-column py-3" xs="12" lg="6">
+                        <Col className="d-flex ms-0 ms-lg-3 flex-column py-3" xs="12" md="6">
                             <div id="box" className="m-auto text-light display-2">
                                 {
                                     moves.map((el, index) => {
@@ -123,7 +122,7 @@ const App = () => {
                                 }
                             </div>
 
-                            <div className={"text-center" + ((gameWon || gameTie) ? "" : " invisible")}>
+                            <div className={"text-center mt-4 mt-md-0" + ((gameWon || gameTie) ? "" : " invisible")}>
                                 <Button size="lg" color="dark" onClick={restart}>Play again</Button>
                             </div>
 
--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,13 +1,13 @@
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap"
 
 const ScoreBoard = ({ score }) => {
-    return <Row className="justify-content-around mx-0">
+    return <Row className="justify-content-between justify-content-md-around align-items-center mx-0 mt-md-auto flex-md-column flex-lg-row">
                 {
                     Object.entries(score).map((entry, index) => {
-                        return <Col className="col-3 p-0" key={index}>
-                                    <Card className="text-center border-0">
-                                        <CardHeader className="bg-dark text-light">{ entry[0].toLocaleUpperCase() }</CardHeader>
-                                        <CardBody className="fs-4 fx-bold py-1">{ entry[1] }</CardBody>
+                        return <Col className="p-0 my-1" key={index} lg="3" md="8" xs="3">
+                                    <Card className="text-center border-0 d-flex flex-column flex-md-row flex-lg-column overflow-hidden">
+                                        <CardHeader className="bg-dark text-light col col-md-5 col-lg-12">{ entry[0].toLocaleUpperCase() }</CardHeader>
+                                        <CardBody className="bg-light fs-4 fx-bold py-1 col col-md-7 col-lg-12">{ entry[1] }</CardBody>
                                     </Card>
                                 </Col>
                     })
